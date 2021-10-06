0c3807ae9526bed8411fcda10f89439af7196ed6
haokai-li <82911890+haokai-li@users.noreply.github.com>
2021-10-05 22:42:35 -0400

    Create code.py

---
+++ code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Haokai Li
+# Created on: Oct 2021
+# This Program is about pybadge
+
+import ugame
+import stage
+
+
+def hello_workd():
+    
+    print("\n\n\n")
+    print("Hello, World!")
+    
+    
+    while True:
+        pass
+
+if __name__ == "__main__":
+    hello_workd()
