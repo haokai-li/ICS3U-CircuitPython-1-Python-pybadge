49d0dbabe746a666a60598622a6bc48908f282c3
haokai-li <82911890+haokai-li@users.noreply.github.com>
2021-10-07 13:23:58 -0400

    #3

--- code.py
+++ code.py
@@ -11,14 +11,29 @@ import stage
 def game_scene():
     # add backgrond
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprite = stage.Bank.from_bmp16("space_aliens.bmp")
+
+    # size
     background = stage.Grid(image_bank_background, 10, 8)
+
+    # sprite frame
+    ship = stage.Grid(image_bank_sprite, 5, 75, 66)
+
+
     # put background in game
     game = stage.Stage(ugame.display, 60)
-    game.layers = [background]
+    game.layers = [ship] + [background]
     game.render_block()
+
     # loop
     while True:
-        pass
+        # get input
+
+        # update game logic
+
+        # redraw Sprite
+        game.render_sprite([ship])
+        game.tick() # wait
 
 if __name__ == "__main__":
     game_scene()
