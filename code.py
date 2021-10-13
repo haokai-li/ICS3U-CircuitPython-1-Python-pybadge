fc9bea847a067e1bb52086aa555fdf213d07fe0d
haokai-li <82911890+haokai-li@users.noreply.github.com>
2021-10-12 23:26:43 -0400

    #4

--- code.py
+++ code.py
@@ -28,7 +28,24 @@ def game_scene():
     # loop
     while True:
         # get input
+        keys = ugame.buttons.get_pressed()
 
+        if keys & ugame.K_X:
+            print("A")
+        if keys & ugame.K_O:
+            print("B")
+        if keys & ugame.K_START:
+            print("Start")
+        if keys & ugame.K_SELECT:
+            print("Select")
+        if keys & ugame.K_RIGHT:
+            ship.move(ship.x + 1, ship.y)
+        if keys & ugame.K_LEFT:
+            ship.move(ship.x - 1, ship.y)
+        if keys & ugame.K_UP:
+            ship.move(ship.x, ship.y - 1)
+        if keys & ugame.K_DOWN:
+            ship.move(ship.x, ship.y + 1)
         # update game logic
 
         # redraw Sprite
