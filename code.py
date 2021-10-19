79b29f591fa4f098c8116171c9cb40201eb4be2a
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-10-19 16:33:35 +0000

    #8

--- code.py
+++ code.py
@@ -29,7 +29,7 @@ def splash_scene():
         image_bank_mt_background, constants.SCREEN_X, constants.SCREEN_Y
     )
 
-    # used this program to split the image into tile:
+    # used this program to split the image into tile :
     # https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
     background.tile(2, 2, 0)  # blank white
     background.tile(3, 2, 1)
