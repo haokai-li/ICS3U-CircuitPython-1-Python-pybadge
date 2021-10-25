cd59291db1253853004e7d4579d8b2e9c2a58c34
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-10-25 16:03:45 +0000

    #12

--- code.py
+++ code.py
@@ -300,32 +300,33 @@ def game_scene():
         for laser_number in range(len(lasers)):
             if lasers[laser_number].x > 0:
                 for alien_number in range(len(aliens)):
-                    if stage.collide(
-                        lasers[laser_number].x + 6,
-                        lasers[laser_number].y + 2,
-                        lasers[laser_number].x + 11,
-                        lasers[laser_number].y + 12,
-                        aliens[alien_number].x + 2,
-                        aliens[alien_number].y,
-                        aliens[alien_number].x + 15,
-                        aliens[alien_number].y + 15,
-                    ):
-                        # you hit an alien
-                        aliens[alien_number].move(
-                            constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
-                        )
-                        lasers[laser_number].move(
-                            constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
-                        )
-                        sound.stop()
-                        sound.play(boom_sound)
-                        show_alien()
-                        show_alien()
-                        score = score + 1
-                        score_text.clear()
-                        score_text.cursor(0, 0)
-                        score_text.move(1, 1)
-                        score_text.text("Score: {0}".format(score))
+                    if aliens[alien_number].x > 0:
+                        if stage.collide(
+                            lasers[laser_number].x + 6,
+                            lasers[laser_number].y + 2,
+                            lasers[laser_number].x + 11,
+                            lasers[laser_number].y + 12,
+                            aliens[alien_number].x + 2,
+                            aliens[alien_number].y,
+                            aliens[alien_number].x + 15,
+                            aliens[alien_number].y + 15,
+                        ):
+                            # you hit an alien
+                            aliens[alien_number].move(
+                                constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+                            )
+                            lasers[laser_number].move(
+                                constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y
+                            )
+                            sound.stop()
+                            sound.play(boom_sound)
+                            show_alien()
+                            show_alien()
+                            score = score + 1
+                            score_text.clear()
+                            score_text.cursor(0, 0)
+                            score_text.move(1, 1)
+                            score_text.text("Score: {0}".format(score))
 
         # redraw Sprite
         game.render_sprites(lasers + [ship] + aliens)
